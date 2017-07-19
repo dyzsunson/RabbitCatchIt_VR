28549b47fb01d51ab464d6495c942449a737e569
LizhouCao <lizhoucao1990@gmail.com>
2017-07-19 17:38:44 -0500

    vr

---
+++ RabbitCatchIt_VR/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    float m_lifeTime = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("End", m_lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.y < -1.1f)
+            End();
+	}
+
+    void End() {
+        Destroy(this.gameObject);
+    }
+}
