80983057d522f3e394d125e1c2d27e5e8ad61f7c
Yangzi Dong <dyzsunson@gmail.com>
2017-07-19 16:41:08 -0500

    Buil scene
    Build the scene. Change the ignore file.

---
+++ RabbitCatchIt_VR/Assets/Scripts/Gun.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+    public GameObject BulletPrefab;
+
+    public Transform GunBodyTransform;
+    public Transform FireTransform;
+    public Transform StartTransform;
+
+    float m_power = 0.0f;
+    float m_max_power = 2.0f;
+    float m_min_power = 0.1f;
+    float m_scaler = 1200.0f;
+    float m_max_reloadTime = 0.5f;
+    float m_reloadTime = 0.0f;
+
+    bool is_reloading = false;
+    float m_reload_gun_speed = 1.0f;
+    Vector3 m_gun_prePosition;
+
+	// Use this for initialization
+	void Start () {
+        m_power = m_min_power;
+        m_gun_prePosition = GunBodyTransform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!is_reloading && Input.GetKey(KeyCode.Space)) {
+            if (m_power < m_max_power)
+                m_power += Time.deltaTime;
+        }
+
+        if (!is_reloading && Input.GetKeyUp(KeyCode.Space)) {
+            Fire();           
+            Reload();
+        }
+
+        if (is_reloading) {
+            if (m_reloadTime > m_max_reloadTime) {
+                ReloadEnd();
+            }
+            else {
+                if (m_reloadTime < m_max_reloadTime / 2.0f) {
+                    GunBodyTransform.Translate(-m_reload_gun_speed * Time.deltaTime * new Vector3(0.0f, 0.0f, 1.0f));
+                }
+                else {
+                    GunBodyTransform.Translate(m_reload_gun_speed * Time.deltaTime * new Vector3(0.0f, 0.0f, 1.0f));
+                }
+                m_reloadTime += Time.deltaTime;
+            }
+            // this.transform.Translate()
+        }
+	}
+
+    void Reload() {
+        m_power = m_min_power;
+        is_reloading = true;
+        m_reloadTime = 0.0f;
+    }
+
+    void ReloadEnd() {
+        GunBodyTransform.localPosition = m_gun_prePosition;
+        is_reloading = false;
+    }
+
+    void Fire() {
+        GameObject bullet = Instantiate(BulletPrefab) as GameObject;
+        bullet.transform.position = FireTransform.position;
+        bullet.GetComponent<Rigidbody>().AddForce(m_scaler * m_power * (FireTransform.position - StartTransform.position).normalized);
+    }
+}
---
+++ RabbitCatchIt_VR/Assets/Scripts/RabbitCannon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitCannon : MonoBehaviour {
+    float m_speed = 0.1f;
+    float m_rotateSpeed = 1.0f;
+    float m_max_degree = 30.0f;
+    float m_degree = 0.0f;
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        /*if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            this.transform.Translate(m_speed * -this.transform.right);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            this.transform.Translate(m_speed * this.transform.right);
+        }*/
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            if (m_degree > -m_max_degree) {
+                this.transform.Rotate(this.transform.up, -m_rotateSpeed);
+                m_degree -= m_rotateSpeed;
+            }
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            if (m_degree < m_max_degree) {
+                this.transform.Rotate(this.transform.up, m_rotateSpeed);
+                m_degree += m_rotateSpeed;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            Fire();
+        }
+    }
+
+    public void Fire() {
+        animator.Play("Base Layer.Hit");
+    }
+}
